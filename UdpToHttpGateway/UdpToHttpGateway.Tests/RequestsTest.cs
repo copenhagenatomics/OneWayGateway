a8e341c2905015a215ef24226b9a91012669c7fa
freddyrios <70888589+freddyrios@users.noreply.github.com>
2024-09-06 23:53:48 +0200

    enabled same code analysis level for the tests project

--- UdpToHttpGateway/UdpToHttpGateway.Tests/RequestsTest.cs
+++ UdpToHttpGateway/UdpToHttpGateway.Tests/RequestsTest.cs
@@ -12,7 +12,10 @@ using UdpToHttpGateway.Client;
 namespace UdpToHttpGateway.Tests;
 
 [TestClass]
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member - This is a false positive, as ms test requires the tests to be public https://github.com/dotnet/roslyn-analyzers/issues/7192
+#pragma warning disable CA1515 // Consider making public types internal - This is a false positive, as ms test requires the tests to be public https://github.com/dotnet/roslyn-analyzers/issues/7192
 public class RequestsTest
+#pragma warning restore CA1515 // Consider making public types internal
 {
     const int RequestTimeout = 1000;
     const int HttpPort = 9080;
@@ -34,7 +37,7 @@ public class RequestsTest
  """);
 
     [TestMethod]
-    public Task TestRootPostRequest() => TestRequest(HttpMethod.Post, "/", [1,2,3,4,5], $"""
+    public Task TestRootPostRequest() => TestRequest(HttpMethod.Post, "/", [1, 2, 3, 4, 5], $"""
  POST http://{testIp}:9080/ HTTP/1.1
  Host: 172.19.253.73:9080
  Content-Length: 5
@@ -52,7 +55,10 @@ public class RequestsTest
  """);
 
     [TestMethod]
-    public Task TestJsonRequest() => TestRequest(HttpMethod.Post, "/", JsonContent.Create(new { MyValues = "abc" }), $"""
+    public Task TestJsonRequest()
+    {
+        using var content = JsonContent.Create(new { MyValues = "abc" });
+        return TestRequest(HttpMethod.Post, "/", content, $"""
  POST http://{testIp}:9080/ HTTP/1.1
  Host: 172.19.253.73:9080
  Content-Type: application/json; charset=utf-8
@@ -60,31 +66,37 @@ public class RequestsTest
 
 
  """);
+    }
 
-    private static Task TestRequest(HttpMethod method, string relativeUri, byte[]? contentBytes, string expectedHeader) =>
-        TestRequest(method, relativeUri, contentBytes != null ? new ReadOnlyMemoryContent(contentBytes) : null, expectedHeader);
-    private static async Task TestRequest(HttpMethod method, string relativeUri, HttpContent? content, string expectedHeader)
+    static Task TestRequest(HttpMethod method, string relativeUri, byte[]? contentBytes, string expectedHeader)
     {
-        Channel<(string, byte[])> received = Channel.CreateUnbounded<(string, byte[])>();
-        HttpRequestMessage request = new(method, new Uri(new Uri($"http://{testIp}:{HttpPort}"), relativeUri));
+        using ReadOnlyMemoryContent? content = contentBytes != null ? new ReadOnlyMemoryContent(contentBytes) : null;
+        return TestRequest(method, relativeUri, content, expectedHeader);
+    }
+
+    static async Task TestRequest(HttpMethod method, string relativeUri, HttpContent? content, string expectedHeader)
+    {
+        var received = Channel.CreateUnbounded<(string, byte[])>();
+        using HttpRequestMessage request = new(method, new Uri(new Uri($"http://{testIp}:{HttpPort}"), relativeUri));
         if (content != null)
             request.Content = content;
         using var gatewayClient = new GatewayClient(IPEndPoint.Parse("127.0.0.1:4280"));
-        await using var web = CreateFakeServer(HttpPort, received);
-        await gatewayClient.Send(request);
-        await AssertFakeServerReceivedExpectedRequest(content != null ? await content.ReadAsByteArrayAsync() : [], expectedHeader, received);
-        await web.StopAsync();
+        WebApplication web = CreateFakeServer(HttpPort, received);
+        await using System.Runtime.CompilerServices.ConfiguredAsyncDisposable _ = web.ConfigureAwait(false);
+        await gatewayClient.Send(request).ConfigureAwait(false);
+        await AssertFakeServerReceivedExpectedRequest(content != null ? await content.ReadAsByteArrayAsync().ConfigureAwait(false) : [], expectedHeader, received).ConfigureAwait(false);
+        await web.StopAsync().ConfigureAwait(false);
     }
 
 
-    private static async Task AssertFakeServerReceivedExpectedRequest(byte[] expectedContent, string expectedHeader, Channel<(string, byte[])> received)
+    static async Task AssertFakeServerReceivedExpectedRequest(byte[] expectedContent, string expectedHeader, Channel<(string, byte[])> received)
     {
         try
         {
             using var cts = new CancellationTokenSource(RequestTimeout);
             //Note the headers string received in the channel is a conversion from the typed headers received by the fake server to string done by test code in ToRaw, so its not exactly what was send by the gateway.
             //However, the header has first been parsed by asp.net, so we get some level of validation of how the headers look for receiving servers.
-            var (headers, content) = await received.Reader.ReadAsync(cts.Token);
+            (string headers, byte[] content) = await received.Reader.ReadAsync(cts.Token).ConfigureAwait(false);
             Assert.AreEqual(expectedHeader, headers, $"Headers did not match. ExpectedToUtf8ToHex: {Convert.ToHexString(Encoding.UTF8.GetBytes(expectedHeader))}. ActualToUtf8ToHex: {Convert.ToHexString(Encoding.UTF8.GetBytes(headers))}");
             CollectionAssert.AreEqual(expectedContent, content, $"Content did not match. ExpectedToHex: {Convert.ToHexString(expectedContent)}. ActualToHex: {Convert.ToHexString(content)}");
         }
@@ -94,33 +106,33 @@ public class RequestsTest
         }
     }
 
-    private static WebApplication CreateFakeServer(int httpPort, Channel<(string, byte[])> received)
+    static WebApplication CreateFakeServer(int httpPort, Channel<(string, byte[])> received)
     {
-        var builder = WebApplication.CreateBuilder();
-        builder.WebHost.UseShutdownTimeout(TimeSpan.FromSeconds(3));//hard stop after 3 seconds.
-        var app = builder.Build();
+        WebApplicationBuilder builder = WebApplication.CreateBuilder();
+        _ = builder.WebHost.UseShutdownTimeout(TimeSpan.FromSeconds(3));//hard stop after 3 seconds.
+        WebApplication app = builder.Build();
         app.Urls.Add($"http://+:{httpPort}");
-        app.Map("/", async (HttpRequest request) =>
+        _ = app.Map("/", async (HttpRequest request) =>
         {
-            await received.Writer.WriteAsync(await ToRaw(request));
+            await received.Writer.WriteAsync(await ToRaw(request).ConfigureAwait(false)).ConfigureAwait(false);
             return "OK";
         });
         _ = app.RunAsync();
         return app;
     }
 
-    public async static Task<(string,byte[])> ToRaw(HttpRequest request)
+    static async Task<(string, byte[])> ToRaw(HttpRequest request)
     {
-        StringWriter writer = new StringWriter();
+        using var writer = new StringWriter();
         WriteStartLine(request, writer);
         WriteHeaders(request, writer);
         using var stream = new MemoryStream();
-        await request.Body.CopyToAsync(stream);
+        await request.Body.CopyToAsync(stream).ConfigureAwait(false);
         stream.Position = 0;
         return (writer.ToString(), stream.ToArray());
     }
 
-    private static void WriteStartLine(HttpRequest request, StringWriter writer)
+    static void WriteStartLine(HttpRequest request, StringWriter writer)
     {
         const string SPACE = " ";
 
@@ -130,9 +142,9 @@ public class RequestsTest
         writer.Write("\r\n");
     }
 
-    private static void WriteHeaders(HttpRequest request, StringWriter writer)
+    static void WriteHeaders(HttpRequest request, StringWriter writer)
     {
-        foreach (var kvp in request.Headers)
+        foreach (KeyValuePair<string, Microsoft.Extensions.Primitives.StringValues> kvp in request.Headers)
         {
             writer.Write(string.Format("{0}: {1}", kvp.Key, kvp.Value));
             writer.Write("\r\n");
@@ -141,3 +153,4 @@ public class RequestsTest
         writer.Write("\r\n");
     }
 }
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
