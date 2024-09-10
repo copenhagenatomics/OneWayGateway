9147d613c305b15b675d3400bf784bb784e41e96
freddyrios <70888589+freddyrios@users.noreply.github.com>
2024-09-10 15:12:29 +0200

    added configure.sh to run as a systemd service (tested on debian)

--- UdpToHttpGateway/UdpToHttpGateway/Program.cs
+++ UdpToHttpGateway/UdpToHttpGateway/Program.cs
@@ -2,6 +2,7 @@ using UdpToHttpGateway;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 builder.Services.Configure<GatewayOptions>(builder.Configuration.GetRequiredSection(nameof(GatewayOptions)));
+builder.Services.AddSystemd();
 builder.Services.AddHostedService<UdpReceiver>();
 
 builder.Build().Run();
