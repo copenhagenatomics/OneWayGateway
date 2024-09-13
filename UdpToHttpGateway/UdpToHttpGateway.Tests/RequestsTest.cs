e52cd2452ad317a07fda6f056b5a62f056ce2087
freddyrios <70888589+freddyrios@users.noreply.github.com>
2024-09-13 13:41:57 +0200

    trying to force docker to be bind port mapping on ipv4

--- UdpToHttpGateway/UdpToHttpGateway.Tests/RequestsTest.cs
+++ UdpToHttpGateway/UdpToHttpGateway.Tests/RequestsTest.cs
@@ -19,7 +19,7 @@ public class RequestsTest
     const int RequestTimeout = 1000;
     const int HttpPort = 9080;
     const string TestAddress = "host.docker.internal";
-    static readonly IPEndPoint GatewayIp = IPEndPoint.Parse("::1:4280");
+    static readonly IPEndPoint GatewayIp = IPEndPoint.Parse("127.0.0.1:4280");
 
     [TestMethod]
     public Task TestRootGetRequest() => TestRequest(HttpMethod.Get, "/", default(byte[]?), $"""
